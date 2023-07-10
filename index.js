37e2779dbac5b6b6e036767d424c4e133ee7378b
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-07-10 10:00:19 +0300

    +

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const invokeAction= async ({ action, id, name, email, phone }) => {
     case 'list':
           const allContacts = await phoneBook.listContacts();
           return console.log(allContacts);
-    //   break;
+    
 
     case 'get':
           const oneContact = await phoneBook.getContactById(id);
           return console.log(oneContact);
-      break;
+      
 
     case 'add':
           const newContact = await phoneBook.addContact(name, email, phone);
           return console.log(newContact);
-      break;
+      
 
     case 'remove':
           const deleteContact = await phoneBook.removeContact(id);
           return console.log(deleteContact);
-    //   break;
+
 
     default:
       console.warn('\x1B[31m Unknown action type!');
@@ -30,13 +30,7 @@ const invokeAction= async ({ action, id, name, email, phone }) => {
 }
 
 
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "rsKkOQUi80UsgVPCcLZZW" });
-// invokeAction({ action: "remove", id: "rsKkOQUi80UsgVPCcLZZW"});
-// invokeAction({ action: "add", name: "ne trogai kozy", email: "renegat@gmail.com", phone:"(777) 888-1111" });
-
-// console.log(process.argv);
 
 const { argv } = yargs(process.argv.slice(2));
-// console.log(argv);
+
 invokeAction(argv);
